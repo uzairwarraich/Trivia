606d2962d9168152d2ecfc20c9d83c5dc0b1dfc8
uzairrwarraich <sunny_uzair91@hotmail.com>
2022-06-29 14:07:43 -0400

    added AppController

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -2,13 +2,20 @@ package com.example.trivia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.os.Bundle;
 
+import com.example.trivia.controller.AppController;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
