53a92ef4edbbb046f20063ac05b7186bddc8fa47
uzairrwarraich <sunny_uzair91@hotmail.com>
2022-07-04 14:33:13 -0400

    Added question bank json request

--- app/src/main/java/com/example/trivia/MainActivity.java
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Application;
 import android.os.Bundle;
 
 import com.example.trivia.controller.AppController;
+import com.example.trivia.data.QuestionBank;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new QuestionBank().getQuestions();
 
 
     }
